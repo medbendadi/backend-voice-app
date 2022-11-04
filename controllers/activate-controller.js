cb14021267b3d8d07fec56df96ce222494ba9ca3
medbendadi <itsbendadi@gmail.com>
2022-11-04 14:13:27 +0100

    Prepare for deployment

--- controllers/activate-controller.js
+++ controllers/activate-controller.js
@@ -15,7 +15,7 @@ class ActivateController {
          avatar.replace(/^(data:image\/(png|jpg|jpeg);base64,)/, ''),
          'base64'
       );
-      imagePath = `${Date.now()}-${Math.round(
+      const imagePath = `${Date.now()}-${Math.round(
          Math.random() * 1e9
       )}.png`;
       // 32478362874-3242342342343432.png
